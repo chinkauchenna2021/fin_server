5b38949c8e47eb966e7e5ead0e91052cf00d558a
chinkauchenna2021 <uchennachinka4@gmail.com>
2025-02-01 13:14:16 +0100

    added entry file code

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();   // Prisma Client
+
+const app: Express = express();
+const port = 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
